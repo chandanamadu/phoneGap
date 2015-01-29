e052540ca4830e74bcc882faac8c3fe8f84f5151
Chandana Madusanka <chandana91madusanka.com>
2015-01-29 14:10:15 +0530

    silly chandana

--- js/silly.js
+++ js/silly.js
@@ -2,8 +2,10 @@
 var SILLY = (function(module){
 
 	module.DoIt = function(resultObiect){
-		resultObiect.prepend(Date()+'<br>');
-
+		//resultObiect.prepend(Date()+'<br>');
+		resultObiect.prepend('chandana'+'<br>');
 	};
 	return module;
 }(SILLY||{}));
+
+
